773b74d5e5fe3d5cbe3f8bc32bfc30c2119b5d1e
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-06 17:27:43 -0700

    first attempt

--- Solution.java
+++ Solution.java
@@ -1,5 +1,27 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list1 = Arrays.stream(nums1).boxed().collect(Collectors.toList());
+        List<Integer> list2 = Arrays.stream(nums2).boxed().collect(Collectors.toList());
+
+        ans.add(findUnique(list1, list2));
+        ans.add(findUnique(list2, list1));
+
+        return ans;
+    }
     
+    public List<Integer> findUnique(List<Integer> list1, List<Integer> list2) {
+        List<Integer> ans = new ArrayList<>();
+        Collections.copy(ans, list1);
+        Iterator<Integer> iterator = ans.iterator();
+        while (iterator.hasNext()) {
+            int num = iterator.next();
+            if (list2.contains(num))
+                iterator.remove();
+        }
+        return ans;
     }
 }
