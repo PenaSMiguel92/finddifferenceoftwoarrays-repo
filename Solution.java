cb33aeffa698f5747023ee7642546157cf644247
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-07 13:45:12 -0700

    faster implementation

--- Solution.java
+++ Solution.java
@@ -4,23 +4,16 @@ import java.util.stream.Collectors;
 public class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list1 = Arrays.stream(nums1).boxed().collect(Collectors.toList());
-        List<Integer> list2 = Arrays.stream(nums2).boxed().collect(Collectors.toList());
+        Set<Integer> s1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> s2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        
+        List<Integer> res1 = new ArrayList<>(s1);
+        res1.removeAll(s2);
+        List<Integer> res2 = new ArrayList<>(s2);
+        res2.removeAll(s1);
 
-        ans.add(findUnique(list1, list2));
-        ans.add(findUnique(list2, list1));
-
+        ans.add(res1);
+        ans.add(res2);
         return ans;
-    }
-    
-    public List<Integer> findUnique(List<Integer> list1, List<Integer> list2) {
-        HashSet<Integer> ans = new HashSet<>();
-        Iterator<Integer> iterator = list1.iterator();
-        while (iterator.hasNext()) {
-            int num = iterator.next();
-            if (!list2.contains(num))
-                ans.add(num);
-        }
-        return ans.stream().toList();
     }
 }
